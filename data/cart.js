55636b8c1088ab1503eef2d8dbfd4bbbe7f4e5c2
Dhanasekar <jdhanasekar1999@gmail.com>
2025-06-08 20:15:45 +0530

    Initial commit - Module creation, add to cart render products via JS

---
+++ data/cart.js
@@ -0,0 +1,21 @@
+export let cart = []
+
+export function addToCart(productId){
+    let matchingItem;
+    cart.forEach((item)=>{
+        if(productId === item.productId){
+            matchingItem = item
+        }
+    })
+    if(matchingItem){
+        matchingItem.quantity ++
+    }
+    else{
+        cart.push(
+            {
+                productId : productId,
+                quantity : 1
+            }
+        )
+    }
+}
