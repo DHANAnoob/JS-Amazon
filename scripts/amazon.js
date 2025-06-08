ab1dd22bf65f527cd298b0b3124a7ba64bb709ed
Dhanasekar <jdhanasekar1999@gmail.com>
2025-06-08 22:01:58 +0530

    Checkout page interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import { cart, addToCart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { moneyFormatter } from './utils/moneyFormatter.js';
 
 let productsHtml = ''
 products.forEach((product) => {
@@ -23,7 +24,7 @@ products.forEach((product) => {
                 </div>
 
                 <div class="product-price">
-                    $${(product.priceCents / 100).toFixed(2)}
+                    $${moneyFormatter(product.priceCents)}
                 </div>
 
                 <div class="product-quantity-container">
---
+++ scripts/utils/moneyFormatter.js
@@ -0,0 +1,3 @@
+export function moneyFormatter(val){
+    return (val/100).toFixed(2);
+}
